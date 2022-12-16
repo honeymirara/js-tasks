6fa00e66bc97ea4d3e2e9e1b77a1dfeb0e82a027
honeymirara <honeymirara@gmail.com>
2022-12-16 23:53:40 +0300

    Tasks

--- numbers/4/app.js
+++ numbers/4/app.js
@@ -1,2 +1,10 @@
 // Есть значение. Добавить проверку на ввод только чисел. Если число, вывести true,
-// в противном случае false (isNaN)
+// в противном случае false (isNa
+const value = prompt();
+
+if (isNaN(value)) {
+    console.log(false);
+} else {
+    console.log(true);
+}
+
