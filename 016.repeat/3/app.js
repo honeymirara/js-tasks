be4346e8fff38b12276631075699beb0eb9dd89a
honeymirara <honeymirara@gmail.com>
2022-12-22 23:59:48 +0300

    task

--- 016.repeat/3/app.js
+++ 016.repeat/3/app.js
@@ -1,7 +1,16 @@
 // На вход подается число. Необходимо вывести все числа от 1 до введенного числа
 // кратные 3. 
 
-const a = 42;
-for (let i = 0; i < 0; i++) {
-    console.log(i);
+let a = 42;
+
+if (!isNaN(a)) {
+    a = +a;
+    console.log(typeof a);
+    for (let i = 0; i < 0; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+} else {
+    console.log("error");
 }
