cef723ab226b888c5eb26afe9fea3c4bf8222427
honeymirara <honeymirara@gmail.com>
2022-12-23 23:51:59 +0300

    tasks

--- daily12/6/app.js
+++ daily12/6/app.js
@@ -1,2 +1,12 @@
 // Найти квадратный корень числа. Если же число без плавающей точки, то просто
 // вывести его. В противном случае необходимо округлить число до целых. Добавить проверку на ввод только чисел
+let value = prompt('Введите значение');
+let sqrt = Math.sqrt(value);
+
+if (isNaN(value)) {
+    console.log("Error");
+
+} else if (Number.isInteger(sqrt)) {
+    console.log(sqrt);
+} else console.log(sqrt.toFixed());
+
