cf8c97d6da4ce3e1444bdf629f6e3f82050f59bd
honeymirara <honeymirara@gmail.com>
2022-12-24 00:29:58 +0300

    tasks

--- daily12/8/app.js
+++ daily12/8/app.js
@@ -1,10 +1,22 @@
 // Пользователь вводит номер месяц. Необходимо вывести пору года по номеру
 // месяца (2 способа)
-let day = prompt('Введите день');
-let month = prompt('Введите месяц');
-if (month <= 2 ) {
+let month = +prompt('Введите месяц');
+
+if (month <= 2 || month === 12) {
     console.log('Зима')
-} else (month = 12){
-    console.log('Зима');
 }
+
+if (month <= 5 && month >= 3) {
+    console.log('Весна');
+}
+
+if (month <= 8 && month >= 6) {
+    console.log('Лето');
+}
+
+if (month <= 11 && month >= 9) {
+    console.log('Осень');
+}
+
+
 
