21bed7b020b9406684da786274066419166a850f
honeymirara <honeymirara@gmail.com>
2022-12-23 23:59:08 +0300

    task

--- daily12/8/app.js
+++ daily12/8/app.js
@@ -2,5 +2,9 @@
 // месяца (2 способа)
 let day = prompt('Введите день');
 let month = prompt('Введите месяц');
-if (month >= 2) console.log('Зима');
-if (month > 2 and month <= 5) console.log('Весна');
+if (month <= 2 ) {
+    console.log('Зима')
+} else (month = 12){
+    console.log('Зима');
+}
+
