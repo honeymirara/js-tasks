ed36631a38066f8fe6a6054ed49ea47703cc7c44
honeymirara <honeymirara@gmail.com>
2022-12-24 23:53:50 +0300

    tasks

--- daily12/9/app.js
+++ daily12/9/app.js
@@ -1,3 +1,23 @@
 // Пользователь вводит число: 1, 2, 3 или 4. Если это значение '1', то вывести в консоль
 // 'зима', '2' – 'весна' и так далее. Добавить проверку на ввод только чисел. Решите
 // задачу через switch-case. 
+const value = +prompt("Enter the number: ");
+
+if (isNaN(value)) {
+    console.log('Error');
+}
+
+switch (value) {
+    case 1:
+        console.log(`${value} -> Зима`)
+        break;
+    case 2:
+        console.log(`${value} -> Весна`)
+        break;
+    case 3:
+        console.log(`${value} -> Лето`)
+        break;
+    case 4:
+        console.log(`${value} -> Осень`)
+        break;
+}
