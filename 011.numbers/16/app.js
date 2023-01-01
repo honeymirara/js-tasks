782685a5c164619dd2fc6012999d536503fa53a6
honeymirara <honeymirara@gmail.com>
2023-01-01 23:07:45 +0300

    task

---
+++ 011.numbers/16/app.js
@@ -0,0 +1,9 @@
+// Пользователь вводит с клавиатуры число. Найдите корень введенного числа (если
+//     же результат – число с плавающей точкой, то округлить до 2 знаков после запятой)
+
+const a = +prompt('Введите значение');
+const sqrt = +Math.sqrt(a);
+
+if (Number.isInteger(sqrt)) {
+    console.log(sqrt);
+} else console.log(sqrt.toFixed());
