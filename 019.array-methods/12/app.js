3343b12cbc248da86cb6eceb80f51d3e805f474e
honeymirara <honeymirara@gmail.com>
2023-01-02 18:22:22 +0300

    tasls

---
+++ 019.array-methods/12/app.js
@@ -0,0 +1,26 @@
+// . На входе n – количество элементов массива. Далее производится заполнение
+// массива с клавиатуры. Необходимо создать новый массив из отфильтрованных
+// значений, где строка начинается на [a, h]. Проверка на ввод только текстовых
+// значений на заполнение массива. filter
+// [“ann”, “school”, “hschool”, “borabora”] -> [“ann”, “hschool”]
+
+// let a = prompt('count of elem');
+// let arr = [];
+
+// for (let i = 0; i < a; i++) {
+//     let elem = prompt('введите значение');
+// }
+// if (isNaN(elem)) {
+//     arr.push(elem);
+// }
+// let filtered = arr.filter(function (el) {
+//     if (el[0] == 'a' || el[0] == 'h') {
+//         return true;
+//     }
+// });
+
+let filtered = arr.filtered(function (el){
+    return el[0] == 'a' || el[0] == 'h';
+
+})
+console.log(filtered);
