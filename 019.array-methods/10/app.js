c2507fd7e9f4fb8f77687e15d0404d33d536c129
honeymirara <honeymirara@gmail.com>
2023-01-05 20:12:30 +0300

    task

--- 019.array-methods/10/app.js
+++ 019.array-methods/10/app.js
@@ -4,3 +4,21 @@
 // [1, 2, 3, 4, 5, 6] -> [“нечет”, “чет”, “нечет”, “чет”, “нечет”, “чет”]
 // [1, 24, 5] -> [“нечет”, “чет”, “нечет”]
 // Использовать map
+
+const n = +prompt('введите кол-во эл-тов массива');
+const arr = [];
+for (let i = 0; i < n; i++) {
+    const elem = prompt();
+    if (isNaN(elem)) continue
+    arr.push(+elem);
+}
+
+const newMap = arr.map(function (el) {
+    if (el % 2 === 0) {
+        return 'чет'
+    } else {
+        return 'неч'
+    }
+})
+
+console.log(newMap);
