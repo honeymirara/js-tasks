df2e23d5dfe3d3b76b29c69eaf378ebd92e5d07d
honeymirara <honeymirara@gmail.com>
2023-01-06 23:00:22 +0300

    task

--- 019.array-methods/11/app.js
+++ 019.array-methods/11/app.js
@@ -1,4 +1,36 @@
 // На входе n – количество элементов массива. Далее производится заполнение
 // массива с клавиатуры. Выведите произведение всех элементов массива.
 // Проверки на ввод только чисел. Использовать forEach, reduce
-let n = +prompt('введите кол-во элементов массива');
+
+
+//1-ый способ
+
+// let n = +prompt('введите кол-во элементов массива');
+// let arr = [];
+// for (let i = 0; i < n; i++) {
+//     let temp = prompt('введите элемент массива');
+//     arr.push(temp);
+
+
+// }
+// let a = 1;
+// arr.forEach(function (elem) {
+//     if (!isNaN(elem)) {
+//         a *= elem;
+//     } else {
+//         console.log('не число')
+//     }
+// })
+// console.log(a);
+
+//2-ой способ
+
+reduce = arr.reduce(function (acc, elem) {
+    if (!isNaN(elem)) {
+        return acc * elem;
+    } else {
+        return acc * 1;
+    }
+}, 1);
+
+console.log(reduce);
