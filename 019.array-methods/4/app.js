ef337295b5a57a8b854f0e15b8126ea5c48377b6
honeymirara <honeymirara@gmail.com>
2023-01-07 18:06:38 +0300

    task

--- 019.array-methods/4/app.js
+++ 019.array-methods/4/app.js
@@ -1,3 +1,10 @@
 // Заполнять массив числами бесконечно до тех пор, пока длина массива не будет
 // равна 5. Вывести массив в консоль (цикл for, while)
-let value = prompt();
+const value = 5;
+let arr = [];
+
+while (arr.length < value) {
+    let n = prompt('Введите элемент');
+    arr.push(n);
+}
+console.log(arr);
