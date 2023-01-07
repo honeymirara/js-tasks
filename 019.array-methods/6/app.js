dd89e55f9bf6f9bd4e6ac2dc8df1e2842d9936ad
honeymirara <honeymirara@gmail.com>
2023-01-08 01:29:21 +0300

    task

--- 019.array-methods/6/app.js
+++ 019.array-methods/6/app.js
@@ -1,12 +1,18 @@
 // На входе n – количество элементов массива. Далее производится заполнение
 // массива с клавиатуры. Необходимо вывести true, если хотя бы 1 элемент массива –
 // число. Some, forEach
+
 const n = prompt('Введите количество эл-тов массива');
 let arr = [];
 
-arr.forEach(function (arr) {
-    let elem = prompt();
-})
-// arr.some(function (arr) {
-//     return true;
-// })
+while (arr.length < n) {
+    let value = prompt('Введите элемент');
+    arr.push(value);
+}
+
+console.log(arr.some(function (element) {
+    return !isNaN(element);
+}));
+
+
+
