272777b9e285072ba98c1c0e84e5ab7d9233f53d
honeymirara <honeymirara@gmail.com>
2023-01-10 22:56:41 +0000

    objects

--- 020.repeat/9/app.js
+++ 020.repeat/9/app.js
@@ -1,14 +1,10 @@
 // На вход программе подается строка текста, вида “xxxx-xx-xx”. Разбить на массив.
 // Преобразуйте эту дату в формат “xx/xx/xxxx”. Как вы думаете, что здесь применить:
 // map, forEach, filter?
+
+
 const str = prompt('').split('');
 const arr = [];
-
-arr.forEach(function (elem) {
-    if (elem === '-') {
-        return elem === '/'
-    }
-})
-
+console.log(str.filter(char => char !== '-').join('/'));
 
 
