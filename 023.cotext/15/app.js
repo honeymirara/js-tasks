d4c07ef1331294f0fdec51aff370d4135f0762f1
honeymirara <honeymirara@gmail.com>
2023-01-18 18:19:36 +0300

    taskj

--- 023.cotext/15/app.js
+++ 023.cotext/15/app.js
@@ -1,12 +1,48 @@
 /* *Вычислите сумму массива целых чисел статичного массива используя рекурсию
  */
+
+/* 1-ый способ
 const array = [1, 2, 3, 4, 5, 6];
 
-function doSum (array_) {
-let num = array_.reduce(function(elem)) {
-    return 
-}
+function doSum(array_) {
+    let num = array_.reduce(function (previousValue, currentValue ) {
+        return previousValue + currentValue;
+    })
+    return num;
+};
+
+let getSum = doSum(array);
+console.log(getSum); */
+
+
+// 2-ой способ
+/* const array = [1, 2, 3, 4, 5];
+
+function doSum(array_) {
+    let result = 0;
+
+    for (let i = 0; i < array_.length; i++) {
+        result += array[i];
+    }
+    return result;
 }
 
-doSum(array_);
+let getSum = doSum(array);
+console.log(getSum);
 
+
+
+ */
+ // 3-ий способ
+
+const array = [1, 2, 3, 4, 5];
+
+function doSum(count){
+    if (count === 1){
+        return count 
+    }
+    return count + doSum(array[i]);
+
+}  
+let result = doSum(count);
+console.log(result);
