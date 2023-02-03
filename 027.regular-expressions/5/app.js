969c46a494673a827876578916bfc440ea5596be
honeymirara <honeymirara@gmail.com>
2023-02-03 23:12:44 +0100

    regex

--- 027.regular-expressions/5/app.js
+++ 027.regular-expressions/5/app.js
@@ -2,13 +2,18 @@
 для данной строки. Если же строка подходит под регулярное выражение, то
 вывести булевое true, в противном случае бросить исключение и обработать */
 
-const string = 'https://supports-cats.com/info.net/';
+const string = 'https://supports-cats.com';
 
 function isValid(string_) {
     try {
+        if (!/^[(http|https|ftp)]+:\/\/[a-z0-9]+[-(a-z0A-Z)*_]+(\.[a-z]+)/gm.test(string_)) throw new Error;
+        return true;
 
     } catch (err) {
         return err.message;
     }
 
 }
+
+let result = isValid(string);
+console.log(result);
