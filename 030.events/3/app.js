a2d23d233887f6c9246ec25cdd77c020e42d40a5
honeymirara <honeymirara@gmail.com>
2023-02-07 01:21:15 +0300

    events

---
+++ 030.events/3/app.js
@@ -0,0 +1,8 @@
+/*  Необходимо отобразить кнопку с надписью «Нажми на меня» и пустой инпут. По
+клику на кнопку вызвать alert и отобразить сообщение из значения инпута
+ */
+
+let button = document.querySelector('.button');
+button.addEventListener('click', function(){
+    
+})
