30af669640d89d6d8bf2a7c136e6c33660742256
honeymirara <honeymirara@gmail.com>
2023-02-10 00:34:29 +0300

    double-input

---
+++ 030.events/6/app.js
@@ -0,0 +1,17 @@
+/* Необходимо отобразить кнопку и инпут со значением по умолчанию. По клику на
+кнопку заменить значение инпута */
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+    try {
+        const inp = document.querySelector('input');
+        if (inp.value == '???') {
+            inp.value = '!!!'
+        } else {
+            inp.value = '???'
+        }
+
+    } catch (err) {
+        alert(err.message);
+    }
+})
