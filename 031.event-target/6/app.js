3fa3d3062acf4fc2987082d6253c3030e5e10629
honeymirara <honeymirara@gmail.com>
2023-02-11 22:36:33 +0300

    motion-pics

--- 031.event-target/6/app.js
+++ 031.event-target/6/app.js
@@ -1,3 +1,12 @@
 /* Добавьте картинку на страницу. При наведении на картинку мышкой необходимо
 ее изменять на другую картинку. Но как только мышка снова отходит в сторону, то
 возвращается первая картинка */
+
+const img = document.querySelector('.img');
+img.addEventListener('mouseover', function () {
+    this.style = 'background-image: url(./elf.jpg)'
+
+})
+img.addEventListener('mouseout', function () {
+    this.style = 'background-image: url(./kawai.jpg)'
+})
