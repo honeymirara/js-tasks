d27bf63df4af6d2e4a5498f655498fda36b8c156
honeymirara <honeymirara@gmail.com>
2023-02-19 12:37:52 +0300

    tasks

--- 035.OOP/3/app.js
+++ 035.OOP/3/app.js
@@ -2,3 +2,32 @@
 reverseString, переворачивающий строку, метод upperFirst, возвращающий строку,
 где первая буква заглавная и метод upperEvery, который делает заглавной первую
 букву каждого слова этой строки. */
+
+class WordString {
+    constructor(string) {
+        this.string = string;
+    }
+
+    reverseString() {
+        return `${this.string.split('').reverse().join('')}`
+    };
+
+    upperFirst() {
+        return `${this.string[0].toUpperCase() + this.string.slice(1)}`;
+
+    };
+
+    upperEvery() {
+        const array = this.string.split(' ');
+        const arrStr = array.map(function (el) {
+            return el[0].toUpperCase() + el.slice(1)
+        });
+        return arrStr.join(' ');
+    };
+}
+
+let wordString = new WordString('hello world!');
+
+console.log(wordString.reverseString());
+console.log(wordString.upperFirst());
+console.log(wordString.upperEvery());
