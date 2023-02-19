d9fd4255d49e115b47c9183b25c6382c3899d445
honeymirara <honeymirara@gmail.com>
2023-02-18 21:04:40 -1200

    tasks

---
+++ 035.OOP/1/app.js
@@ -0,0 +1,34 @@
+/* Реализуйте класс Singer, который будет иметь следующие свойства: name,
+surname. Также класс должен иметь метод getАutograph, который будет выводить
+“{name} {surname), с наилучшими пожеланиями” */
+
+/* class Singer {
+
+    getAutograph(name, surname) {
+
+        return (`${name} ${surname}, with best wishes`);
+    }
+
+}
+
+let singer = new Singer();
+
+console.log(singer.getAutograph('Robert', 'Smith')); */
+
+
+//2
+
+class Singer {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    getAutograph() {
+        return (`${this.name} ${this.surname}, best wishes`);
+    }
+}
+
+let singer = new Singer('Robert', 'Smith');
+console.log(singer.getAutograph());
+
