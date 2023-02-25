2916b5e7b93489313fb410aa4ac3309f894dc149
honeymirara <honeymirara@gmail.com>
2023-02-25 10:39:44 -1000

    task-repeat

---
+++ 030.events/8/app.js
@@ -0,0 +1,8 @@
+/* У вас есть кнопка. После нажатия поменять background. */
+const buttonTag = document.querySelector('button');
+
+buttonTag.addEventListener('click', function () {
+
+    buttonTag.style = 'background-color: pink';
+
+});
