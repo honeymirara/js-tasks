226eeb48806e74b4e20908fe470fa6aa125fbf42
honeymirara <honeymirara@gmail.com>
2023-02-26 10:51:38 -1000

    two-button

---
+++ 030.events/9/app.js
@@ -0,0 +1,12 @@
+/* После нажатия на кнопку «Разблокировать» мы можем вносить изменения в
+инпут. После нажатия на заблокировать данное действие запрещается, тем самым
+выключая возможность редактирования значения инпута */
+
+const inputTag = documnet.querySelector('input');
+const unlockButtonTag = documnet.querySelector('.unlock');
+const lockButtonTag = documnet.querySelector('.lock');
+
+unlockButtonTag.addEventListener('click', function(){
+    
+})
+
