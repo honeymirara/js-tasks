600d8224c76ab54b8b761bc99041933bfb164751
honeymirara <honeymirara@gmail.com>
2023-02-28 13:39:55 -1000

    do-up-task

--- 030.events/11/app.js
+++ 030.events/11/app.js
@@ -1,2 +1,18 @@
 /* Вывести в строчку все вводимые значения через инпут в виде массива. Добавить
 проверку, что поле не пустое */
+const inputTag = document.querySelector('input');
+const buttonTag = document.querySelector('button');
+const resultTag = document.querySelector('.result');
+
+buttonTag.addEventListener('click', function () {
+    try {
+        if (!inputTag.value) throw new Error("input is empty");
+
+
+        let arrayResult = inputTag.value.join('');
+        inputTag.value.innerHTML = arrayResult;
+
+    } catch (err) {
+        alert(err.message);
+    }
+})
