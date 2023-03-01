5c34d4c844c54ffad6d165a3a0d5b80cebd0be2f
honeymirara <honeymirara@gmail.com>
2023-03-01 11:39:01 +0300

    tasks

--- 037.inheritance/3/app.js
+++ 037.inheritance/3/app.js
@@ -1,9 +1,11 @@
-/* Реализуйте класс Singer, который будет наследовать от класса Worker. Класс
-Worker состоит из: свойства Name, свойства Surname, метода GetFullName(), с
-помощью которого можно вывести одновременно имя и фамилию; Класс Singer
-наследует Name, Surname, также состоит из свойств Years, Birth, Style и метода
-GetMoreInfo(), который выводит информацию о певце: Years, Birth, Style.
-Необходимо вызвать метод GetMoreInfo */
+/* Реализуйте класс Singer, который будет наследовать от класса Worker. 
+Класс Worker состоит из: свойства Name,
+свойства surname, метода getFullName(),
+с помощью которого можно вывести одновременно имя и фамилию; 
+Класс Singer состоит из свойств years, birth, style и метода getMoreInfo(), 
+который выводит информацию о певце: name, surname years, birth, style. 
+Необходимо вызвать метод getMoreInfo */
+
 
 class Worker {
     name;
@@ -12,7 +14,14 @@ class Worker {
 
     getFullName() {
         return this.name + ` ` + this.surname;
+    }
 
+    setName(name) {
+        this.name = name;
+    }
+
+    setSurname(surname) {
+        this.surname = surname;
     }
 
 }
@@ -20,5 +29,25 @@ class Worker {
 class Singer extends Worker {
     years;
     birthday;
+    style;
+    getMoreInfo() {
+        return this.name + ` ` + this.surname + ` ` + this.years + this.birthday + `` + this.style;
+    }
 
+    setYears(years) {
+        this.years = years;
+    }
+
+    setBirthday(birthday) {
+        this.birthday = birthday;
+    }
+
+    setStyle(style) {
+        this.style = style;
+    }
 }
+
+
+let singer = new Singer();
+let worker = new Worker();
+
