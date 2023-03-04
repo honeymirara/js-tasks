55e69c41632ab96f1785b665074b758866816dea
honeymirara <honeymirara@gmail.com>
2023-03-04 21:54:58 +0300

    tasks

--- 031.event-target/2/app.js
+++ 031.event-target/2/app.js
@@ -1,1 +1,17 @@
+/* По наведению на кнопку вывести сообщение «Hi». Когда мышка выходит за
+границы кнопки сновка скрывать
+ */
+
+const buttonTag = document.querySelector('button');
+const messageTag = document.querySelector('.message');
+
+buttonTag.addEventListener('mousemove', function () {
+    try {
+
+        messageTag.innerHTML('Hi');
+    } catch (err) {
+        alert(err.message);
+    }
+
+})
 
