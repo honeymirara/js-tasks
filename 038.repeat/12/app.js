547a51cdf1d16385da5d6c13391f815a13af52c0
honeymirara <honeymirara@gmail.com>
2023-03-08 13:04:05 +0300

    task

---
+++ 038.repeat/12/app.js
@@ -0,0 +1,33 @@
+/* Полиморфизм. Реализуйте класс NumberArray. NumberArray хранит функцию
+multArr, которая заполняет и возвращает массив из 5 рандомных элементов.
+Класс ConsoleArray является производным по отношению к базовому,
+родительскому NumberArray и содержит функцию multArr. Необходимо
+переопределить multArr из базового класса записав в переменную используя
+метод super. Далее посчитать произведение всех элементов массива используя
+reduce */
+
+class NumberArray {
+    multArr() {
+        let arr = [];
+        for (let i = 0; i < 5; i++) {
+            const element = Math.round(Math.random() * 100);
+            arr.push(element);
+        };
+        console.log(arr);
+        return arr;
+    }
+}
+
+class ConsoleArray extends NumberArray {
+    multArr() {
+        let arr = super.multArr();
+        return arr.reduce((cur, next) => {
+            return cur * next;
+        }, 1)
+    }
+}
+
+
+let consoleArray = new ConsoleArray();
+console.log(consoleArray.multArr());
+
