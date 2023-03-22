a55cb4e3500767e3885403821520efd81362cbb3
honeymirara <honeymirara@gmail.com>
2023-03-22 07:57:49 -0900

    server-task

--- 036.solid/3/app.js
+++ 036.solid/3/app.js
@@ -10,6 +10,26 @@ repository – функция, симулирующая БД. Хранит ма�
 Задание:
 Необходимо вывести в консоль весь массив */
 
-class ServerGetAll{
-    
+class ServerGetAll {
+
+    controller(object) {
+        try {
+
+
+        } catch (err) {
+            return err.message;
+        }
+    };
+    service(object){
+
+    };
+    repository(object) {
+        const array = [
+        { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
+        { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
+        { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
+        { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
+        { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
+        ];
+    };
 }
