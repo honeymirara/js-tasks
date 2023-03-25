66e859d7b3256d77cef29809765837a998e93091
honeymirara <honeymirara@gmail.com>
2023-03-25 11:07:13 -1100

    number-array

--- 018.сycles/1/app.js
+++ 018.сycles/1/app.js
@@ -1,1 +1,12 @@
 // Выведите столбец чисел от 1 до 50. for, while
+// const arr = [];
+// for (let i = 1; i <= 50; i++) {
+//     arr.push();
+// }
+
+// console.log(arr);
+let i = 1;
+while (i <= 50) {
+    console.log(i)
+    i++
+}
