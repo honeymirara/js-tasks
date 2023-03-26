d961a4094009c41e3f406df4ee78611a70c3ef22
honeymirara <honeymirara@gmail.com>
2023-03-26 11:22:05 -1100

    task

---
+++ 018.сycles/2/app.js
@@ -0,0 +1,17 @@
+// [1, 2, 3, 4, 5]. Выведите каждый элемент массива 3 любыми циклами
+const arr = [];
+
+// arr.length = 5;
+
+// for (let i = 0; i <= arr.length; i++) {
+//     arr.push(arr[i]);
+// }
+// console.log(arr);
+
+// вариант 1
+
+// let i =1;
+// while (i<=5){
+//     console.log(i);
+//     i++
+// }
