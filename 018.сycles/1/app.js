2f2ef64c8a832570997709d241b40b55394b0308
honeymirara <honeymirara@gmail.com>
2023-03-28 07:23:39 -0900

    cycles

--- 018.сycles/1/app.js
+++ 018.сycles/1/app.js
@@ -5,8 +5,17 @@
 // }
 
 // console.log(arr);
-let i = 1;
-while (i <= 50) {
-    console.log(i)
-    i++
+// let i = 1;
+// while (i <= 50) {
+//     console.log(i)
+//     i++
+// }
+
+for (let i = 1; i <= 50; i++) {
+    console.log(i);
+}
+let j = 1;
+while (j < 50) {
+    console.log(j);
+    j++
 }
--- 018.сycles/2/app.js
+++ 018.сycles/2/app.js
@@ -15,3 +15,8 @@ const arr = [];
 //     console.log(i);
 //     i++
 // }
+
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i<=arr; i++){
+    
+}
