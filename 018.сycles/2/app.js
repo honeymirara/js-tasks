4f36a6c04e2e21290b75749a3457f0977989f7a2
honeymirara <honeymirara@gmail.com>
2023-03-29 05:16:04 -0900

    two-methods_of_cycles

--- 018.сycles/2/app.js
+++ 018.сycles/2/app.js
@@ -1,5 +1,5 @@
 // [1, 2, 3, 4, 5]. Выведите каждый элемент массива 3 любыми циклами
-const arr = [];
+
 
 // arr.length = 5;
 
@@ -17,6 +17,14 @@ const arr = [];
 // }
 
 let arr = [1, 2, 3, 4, 5];
-for (let i = 0; i<=arr; i++){
-    
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+for (let j of arr) {
+    console.log(j);
+}
+let i = 0;
+while (i < arr.length) {
+    console.log(arr[i]);
+    i++
 }
