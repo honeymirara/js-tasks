38721c9df848529f1d7d70659b456cdc26af8876
honeymirara <honeymirara@gmail.com>
2023-03-31 05:29:54 -0900

    do-while-option

--- 018.сycles/3/app.js
+++ 018.сycles/3/app.js
@@ -1,9 +1,19 @@
 // Дан массив с элементами [2, 3, 4, 5]. С помощью 3 циклов найдите произведение
 // элементов этого массива.
 
+// const arr = [2, 3, 4, 5];
+// let sum = 1;
+// for (let i = 0; i < arr.length; i++) {
+//     sum *= arr[i];
+// }
+// console.log(sum);
+
 const arr = [2, 3, 4, 5];
 let sum = 1;
-for (let i = 0; i < arr.length; i++) {
+let i = 0;
+
+do {
     sum *= arr[i];
-}
+    i++;
+} while (i < arr.length)
 console.log(sum);
