285a2f6feebe4a8fd5578955428054e2aa87b7c0
honeymirara <honeymirara@gmail.com>
2023-03-31 08:04:25 -0900

    task-edits

--- 018.сycles/11/app.js
+++ 018.сycles/11/app.js
@@ -1,7 +1,12 @@
 // 11. У вас есть массив из строк, чисел и иных примитивов. Необходимо создать новый
 // массив, заполненный только числами первоначального массива
 const arr = [1, 2, 6, 'dvsf', 5, true, null, 'sfdsj'];
+const final_arr = [];
 
 for (let i = 0; i < arr.length; i++){
-    if
+    if (typeof arr[i] === 'number'){
+        final_arr.push(arr[i]);
+    }
 }
+
+console.log(final_arr);
