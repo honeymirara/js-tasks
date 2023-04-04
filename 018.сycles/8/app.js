c77ec8f89fff83e4b04971901f81eb41891b4a56
honeymirara <honeymirara@gmail.com>
2023-04-04 07:15:49 -0900

    cycles

--- 018.сycles/8/app.js
+++ 018.сycles/8/app.js
@@ -4,10 +4,14 @@
 const arr = [1, 2, 2, 3, 4, 5, 5, 2, 4, 5];
 const final_arr = [];
 
-for (let i = 0; i<arr.length; i++){
-    if(!final_arr.includes(arr[i])){
-        final_arr.push(arr[i]);
-    }
-}
+// for (let i = 0; i<arr.length; i++){
+//     if(!final_arr.includes(arr[i])){
+//         final_arr.push(arr[i]);
+//     }
+// }
 
-console.log(arr);
+// console.log(arr);
+
+for (let elem of arr)
+
+
---
+++ 018.сycles/9/app.js
@@ -0,0 +1,11 @@
+// 9. Дан массив 2, 5, 9, 15, 0, 4. Выведите все числа кратные 3 циклом for of
+const arr = [2, 5, 9, 15, 0, 4];
+const new_arr = [];
+
+for (let elem of arr) {
+    if (elem % 3 = 0 ) {
+        new_arr.push(elem);
+    }
+}
+
+console.log(new_arr);
