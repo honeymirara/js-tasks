a0601b16b6bdc770e3607e64a630d50ac276c7a0
honeymirara <honeymirara@gmail.com>
2023-04-04 07:09:24 -0900

    add-method

---
+++ 018.сycles/8/app.js
@@ -0,0 +1,13 @@
+// 8. Дан массив с элементами [1, 2, 2, 3, 4, 4, 3, 4, 5]. Выведите каждый элемент массива
+// без повторений -> [1, 2, 3, 4, 5] (for, for of)
+
+const arr = [1, 2, 2, 3, 4, 5, 5, 2, 4, 5];
+const final_arr = [];
+
+for (let i = 0; i<arr.length; i++){
+    if(!final_arr.includes(arr[i])){
+        final_arr.push(arr[i]);
+    }
+}
+
+console.log(arr);
