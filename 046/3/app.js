e7cab0f34af6ea16178018c55f32cb7c460c625c
honeymirara <honeymirara@gmail.com>
2023-04-05 11:41:45 +0300

    increment-edits

--- 046/3/app.js
+++ 046/3/app.js
@@ -6,5 +6,23 @@
 // • По клику с текстом на кнопку -1 необходимо отнять из счетчика -1. 
 
 class HTML{
-    bindOptionPlus
+    bindOptionPlus(){
+        let displayTag = document.querySelector('.display');
+        let btnPlusTag = document.querySelector(".plus");
+    btnPlusTag = add.EventListener('click', ()=>{
+        displayTag.innerHTML = +displayTag.textContent+1;
+    })
+    }
+
+    bindOptionMinus(){
+        let displayTag = document.querySelector(".display");
+        let btnMinusTag = document.querySelector(".minus");
+    btnMinusTag = add.EventListener('click', ()=>{
+        displayTag.innerHTML = +displayTag.textContent-1;
+    })
+    }
 }
+
+let html = new HTML();
+html.bindOptionPlus();
+html.bindOptionMinus();
