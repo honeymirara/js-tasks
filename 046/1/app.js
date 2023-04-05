51743ef62db50492a966b3fdf518dd37ab6a2ac2
honeymirara <honeymirara@gmail.com>
2023-04-05 11:15:49 +0300

    repeat-oop

---
+++ 046/1/app.js
@@ -0,0 +1,34 @@
+// Реализуйте класс HTML. Класс содержит метод bindOption содержащий обработчик
+// события. Задача: 
+// По клику на кнопку необходимо получить значение input и отобразить в пустой div
+// (в созданной html разметке) посредством InnerHTML.
+// • Добавить стили для отображения текста input: например, изменить цвет букв.
+// • Добавить проверки на получение значения input
+
+
+class HTML {
+    bindOption() {
+        
+        const buttonTag = document.querySelector("button");
+        const inputTag = document.querySelector("input");
+        const outPutTag = document.querySelector(".outPut");
+        buttonTag.addEventListener('click', () => {
+            try{
+                if(inputTag.value.trim() == ''){
+                    throw new Error('value is empty')
+                }
+                outPutTag.innerHTML = inputTag.value;
+                outPutTag.style = 'color:red';
+
+            }catch(err){
+                alert(err.message);
+            }
+           
+            
+        })
+    }
+}
+
+const html = new HTML();
+html.bindOption();
+
