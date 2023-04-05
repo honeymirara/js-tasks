4a14878aa17c71953e8ce9643d5b61efc8617b34
honeymirara <honeymirara@gmail.com>
2023-04-05 11:30:33 +0300

    edits

--- 046/2/app.js
+++ 046/2/app.js
@@ -2,3 +2,16 @@
 // события. HTML разметка содержит кнопку button с начальным значением 0. Задача:
 // • По клику на кнопку каждый раз добавлять +1 в текст кнопки. (Если первоначальный
 // текст кнопки – 0, то после пятого, например, клика значение input – 5)
+
+class HTML {
+    bindOption() {
+        const btn = document.querySelector('button');
+        btn.addEventListener('click', () => {
+            btn.textContent = +btn.textContent + 1
+            btn.style = 'background-color: red'
+        })
+    }
+}
+
+const html = new HTML();
+html.bindOption();
