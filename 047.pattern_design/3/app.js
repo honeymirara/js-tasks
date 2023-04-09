b69dfa6d8e0022971d972ce22544e485fe1fca3c
honeymirara <honeymirara@gmail.com>
2023-04-09 13:00:18 +0300

    unique

--- 047.pattern_design/3/app.js
+++ 047.pattern_design/3/app.js
@@ -1,3 +1,23 @@
 /* Вы вводите числа в поле для ввода формируя и отображая массив.
 Необходимо также отображать только уникальные значения массива
 [1, 1, 4, 2, 3, 3] -> [4, 2] */
+
+const inp = document.querySelector(".inp");
+const buttonTag = document.querySelector("button");
+const arrHTMLTag = document.querySelector(".arr");
+const uniqueTag = document.querySelector(".unique");
+
+let arr = [];
+buttonTag.addEventListener("Click", ()=> {
+    let arrUnique = [];
+    arr.push(inp.value);
+    arrHTMLTag.innerHTML = ` массив ${arr}`;
+
+    for(let i = 0; i<arr.length; i++){
+        if (arr[i] != arr[i +1] && arr[i] != arr[i-1]){
+            arrUnique.push(arr[i])
+        }
+    }
+
+    uniqueTag.innerHTML = `unique : ${arrUnique}`;
+})
