435bfd7a927b5a8d9d41f9988387cb87e1cc2a7a
honeymirara <honeymirara@gmail.com>
2023-04-09 23:37:50 +0300

    concat

--- 047.pattern_design/2/app.js
+++ 047.pattern_design/2/app.js
@@ -2,8 +2,8 @@
 Необходимо отобразить массив, а также отдельно
 все четные элементы этого массива и нечетные */
 
-const buttonTag = document.querySelector('button');
-const inputTag = document.querySelector('inpit');
+/* const buttonTag = document.querySelector('button');
+const inputTag = document.querySelector('input');
 const arrayTag = document.querySelector('.array');
 const chetArrTag = document.querySelector('.chetArrTag');
 const everyInp = [];
@@ -13,4 +13,24 @@ buttonTag.addEventListener('click', () => {
     arrayTag.innerHTML = everyInp;
     const filtered = everyInp.filter(el => el % 2 === 0);
     chetArrTag.innerHTML = filtered;
-});
+}); */
+
+
+
+/* Вы вводите числа в поле для ввода формируя массив.
+Необходимо отобразить массив, а также отдельно
+все четные элементы этого массива и нечетные */
+
+const buttonTag = document.querySelector('button');
+const inputTag = document.querySelector('input');
+const arrChetTag = document.querySelector('.arrChetTag');
+const arrTag = document.querySelector('.array')
+const array = [];
+
+buttonTag.addEventListener("click", ()=>{
+    array.push(inputTag.value);
+     arrChetTag.innerHTML = array;
+     const filtered = array.filter(el => el % 2 ===0);
+     array.innerHTML = filtered;
+    
+})
--- 047.pattern_design/5/app.js
+++ 047.pattern_design/5/app.js
@@ -1,1 +1,11 @@
 /* У вас есть 3 инпута. Необходимо вывести в результат конкатенацию всех инпутов */
+const inpTagOne = document.querySelector(".first-input");
+const inpTagTwo = document.querySelector(".second-input");
+const inpTagThree = document.querySelector(".third-input");
+const buttonTag = document.querySelector("button");
+const outPutTag = document.querySelector(".outPut")
+
+buttonTag.addEventListener("click", () => {
+    outPutTag.innerHTML = inpTagOne.value + inpTagTwo.value + inpTagThree.value;
+})
+
