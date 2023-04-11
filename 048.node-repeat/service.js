cd2db979829e04e782c5dc0fe5afd3ceab26c690
honeymirara <honeymirara@gmail.com>
2023-04-11 00:05:26 -1100

    repeat

---
+++ 048.node-repeat/service.js
@@ -0,0 +1,12 @@
+const arr = [
+    { "id": 1, "name": "Yesenia", "age": 22 },
+    { "id": 2, "name": "Hanna", "age": 22 },
+    { "id": 3, "name": "Stanislau", "age": 25 },
+    { "id": 4, "name": "German", "age": 18 },
+    { "id": 5, "name": "Maria", "age": 27 }
+];
+
+function getDataById(id){
+const filtered = filter(el => el.id == +id);
+return filtered;
+}
