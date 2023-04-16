9e302f7806e9a93e7224d1c31a5ce0ff35878066
honeymirara <honeymirara@gmail.com>
2023-04-16 03:59:56 -1100

    requests

--- 048.nodemon-repeat/index.js
+++ 048.nodemon-repeat/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+
+
+app.get('/', (req, res) => {
+    res.send('it is a get-request');
+});
+
+app.post('/', (req, res) => {
+
+})
 app.listen(3000, () => {
     console.log('server is running');
 })
