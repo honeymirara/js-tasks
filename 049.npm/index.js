9f1835b15e1633c69c506fa3da384ec4afc667dd
honeymirara <honeymirara@gmail.com>
2023-04-17 13:32:24 +0300

    server-get

--- 049.npm/index.js
+++ 049.npm/index.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { getAll, getById, create} = require('./service.js');
+
+
 const app = express();
 
-app.get('/', function (req, res){
-    res.send('it is get-requesrt');
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    const data = getAll();
+    res.send(data);
 })
 
-app.post('/', function(req, res){
-    res.send('it is post')
+app.post('/', function (req, res) {
+    const { name, age } = req.body;
+    const data = create(name, age);
+    res.send(data);
 })
 
-app.listen(3000, function(){
+app.get('/:id', function (req, res) {
+    const { id } = req.params;
+    const element = getById(id);
+    res.send(element);
+})
+
+
+
+app.listen(3000, function () {
     console.log('server is running');
 })
