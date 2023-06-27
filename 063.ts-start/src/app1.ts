87280c289fe153c32dc433d350cf1754da2b94d6
honeymirara <honeymirara@gmail.com>
2023-06-27 11:29:01 +0300

    new-tasks

---
+++ 063.ts-start/src/app1.ts
@@ -0,0 +1,10 @@
+//   Напишите программу, которая проверяет, является ли заданное число num
+// четным или нечетным.
+
+let num1: number = +prompt();
+
+if (num1 % 2 === 0) {
+    console.log('chetny');
+} else {
+    console.log('nechetny');
+}
