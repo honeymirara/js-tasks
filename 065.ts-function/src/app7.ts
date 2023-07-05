dbe6a5a73da2c72111423d1c8252bc31c30b3d40
honeymirara <honeymirara@gmail.com>
2023-07-05 23:06:26 +0300

    repeat

--- 065.ts-function/src/app7.ts
+++ 065.ts-function/src/app7.ts
@@ -1,7 +1,7 @@
 /* У вас есть массив объектов вида приведенного в приложении. Необходимо
-вывести средний прайс среди всех продуктов */
+вывести тот товар, где количество * прайс является наибольшим значением */
 
-interface iThings6 {
+interface iThings7 {
     id: number;
     title: string;
     count: number;
@@ -9,17 +9,10 @@ interface iThings6 {
 }
 
 
-let array6: iThings6[] = [
+let array7: iThings7[] = [
     { id: 1, title: 'Часы', count: 10, price: 500 },
     { id: 2, title: 'Смартфон', count: 33, price: 1500 },
     { id: 3, title: 'Моноблок', count: 6, price: 2200 },
     { id: 4, title: 'Ноутбук', count: 13, price: 3000 },
     { id: 5, title: 'Планшет', count: 22, price: 2100 },
 ];
-
-function middlePrice6(array:iThings6[]):number {
-    let sum6= array.reduce((sum:number, elem:iThings6) => sum + elem.price, 0)
-    return sum6 / array6.length;
-}
-
-console.log(middlePrice6(array6));
