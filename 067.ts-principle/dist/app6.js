6c88011cba6ab87f9c0bcb7da6c348565c41d058
honeymirara <honeymirara@gmail.com>
2023-07-09 13:36:56 +0300

    abstract-ts-interfeca

--- 067.ts-principle/dist/app6.js
+++ 067.ts-principle/dist/app6.js
@@ -2,42 +2,25 @@
 (хначение инициализируется через конструктор класса), методы getAverage,
 getMax, getMin для вычисления среднего значения, максимального и
 минимального элементов массива. */
-
-abstract class ArrayAnalyzer_abstr {
-    abstract array: number[];
+class ArrayAnalyzer_abstr {
 }
-
-interface iArrayAnalyzer{
-    array:number[];
-    getAverege():number;
-    getMax():number;
-    getMin():number;
-}
-
-class ArrayAnalyzer extends ArrayAnalyzer_abstr implements iArrayAnalyzer{
-    array: number[];
-
-    constructor(array: number[]) {
+class ArrayAnalyzer extends ArrayAnalyzer_abstr {
+    array;
+    constructor(array) {
         super();
-        this.array = array
+        this.array = array;
     }
-
-    getAverege(): number {
-       return  this.array.reduce((acc: number, el: number) => acc + el, 0) / this.array.length;
+    getAverege() {
+        return this.array.reduce((acc, el) => acc + el, 0) / this.array.length;
     }
-
-    getMax(): number {
-        return Math.max(...this.array)
+    getMax() {
+        return Math.max(...this.array);
     }
-
-    getMin(): number {
+    getMin() {
         return Math.min(...this.array);
-
     }
 }
-
 let arrayAnalyzer = new ArrayAnalyzer([1, 2, 3, 4, 5, 6, 7, 8]);
 console.log(arrayAnalyzer.getAverege());
 console.log(arrayAnalyzer.getMax());
 console.log(arrayAnalyzer.getMin());
-
