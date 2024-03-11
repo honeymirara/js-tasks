00ed809995c92644f673714f95cc0fc222c65116
honeymirara <honeymirara@gmail.com>
2024-03-11 10:12:11 -0900

    make task15

--- 086.exercise/task15/app.js
+++ 086.exercise/task15/app.js
@@ -2,6 +2,57 @@
 возрасту. На выходе получить объект, где ключ – возраст, а значение – массив
 студентов */
 
-function groupStudentsByAge(){
-    
+/* function groupStudentsByAge(students) {
+    const resultObj = {};
+
+    for (const student of students) {
+        const age = student.age;
+        if (!resultObj[age]) {
+            resultObj[age] = [];
+        }
+        resultObj[age].push(student);
+    }
+
+    return resultObj;
 }
+
+
+
+console.log(groupStudentsByAge(students)); */
+
+
+const students = [
+    { name: 'alex', age: 20 },
+    { name: 'bob', age: 22 },
+    { name: 'charlie', age: 20 },
+    { name: 'david', age: 22 }
+];
+
+/* const res ={}
+
+for (const el of students){
+if(res[el.age]){
+    res[el.age].push(el)
+}else{
+    res[el.age] = [el]
+}
+}
+
+console.log(res) */
+
+
+//3-ий способ
+
+const groupStudentsByAge = (arr) => {
+    let resultObj = {}
+    arr.forEach(el => {
+        if (!resultObj[el.age]) {
+            resultObj[el.age] = [el]
+        } else {
+            resultObj[el.age].push(el)
+        }
+    })
+}
+
+
+return resultObj
